a3de2777761583a3a174060e00b7033952e878bf
Katarzyna Bak <Katarzyna_Bak@epam.com>
2016-04-29 12:44:53 +0200

    created functional pattern

---
+++ app/js/patternFunc.js
@@ -0,0 +1,59 @@
+function patterF() {
+
+  'use strict';
+
+  function Plant(arg) {
+    var that = {};
+    that.pattern = "functional";
+    that.species = arg.name;
+    that.price = arg.price;
+    that.getPrice = function () {
+      return this.price;
+    };
+    return that;
+  }
+
+  /**************** Tree ***************/
+
+  function Tree(arg) {
+    var that = Plant(arg);
+    that.leafs = arg.leafs || null;
+    that.dust_time = arg.dust || null;
+    that.fruits = arg.fruits || null;
+    that.flowers = arg.flowers || null;
+
+    that.isDustingIn = function (month) {
+      return this.dust_time === month ? true : false;
+    };
+
+    return that;
+  }
+
+  /**************** Flower ***************/
+
+  function Flower(arg) {
+    var that = Plant(arg);
+    that.color = arg.color;
+    that.life_cycle = arg.life_cycle;
+    that.bloom_time = arg.bloom;
+    that.eviroment = arg.env;
+
+    that.isGardenFlower = function () {
+      return this.eviroment.includes('garden') ? true : false;
+    };
+
+    that.isHomeFlower = function () {
+      return this.eviroment.includes('garden') ? true : false;
+    };
+
+    return that;
+  }
+
+  return {
+    Plant: Plant,
+    Tree: Tree,
+    Flower: Flower
+  };
+
+}
+
--- app/js/patternProto.js
+++ app/js/patternProto.js
@@ -1,51 +1,64 @@
-function inherits(Parent, Child) {
-  function F() {}
-  F.prototype = Parent;
-  Child.prototype = new F();
+//(function () {
+function patternP() {
+  'use strict';
+
+  function inherits(Parent, Child) {
+    function F() {}
+    F.prototype = Parent;
+    Child.prototype = new F();
+  }
+
+  function Plant(arg) {
+    this.pattern = "prototypal";
+    this.species = arg.name;
+    this.price = arg.price;
+  }
+
+  Plant.getPrice = function () {
+    return this.price;
+  };
+
+  /**************** Flower ***************/
+
+  function Tree(arg) {
+    Plant.call(this, arg);
+    this.leafs = arg.leafs || null;
+    this.dust_time = arg.dust || null;
+    this.fruits = arg.fruits || null;
+    this.flowers = arg.flowers || null;
+  }
+
+  inherits(Plant, Tree);
+
+  Tree.prototype.isDustingIn = function (month) {
+    return this.dust_time === month ? true : false;
+  };
+
+  /**************** Flower ***************/
+
+  function Flower(arg) {
+    Plant.call(this, arg);
+    this.color = arg.color;
+    this.life_cycle = arg.life_cycle;
+    this.bloom_time = arg.bloom;
+    this.eviroment = arg.env;
+  }
+
+  inherits(Plant, Flower);
+
+  Flower.prototype.isGardenFlower = function () {
+    return this.eviroment.includes('garden') ? true : false;
+  };
+
+  Flower.prototype.isHomeFlower = function () {
+    return this.eviroment.includes('garden') ? true : false;
+  };
+
+  return {
+    Plant: Plant,
+    Tree: Tree,
+    Flower: Flower
+  };
+
 }
-
-function Plant(arg) {
-  this.species = arg.name;
-  this.price = arg.price;
-}
-
-Plant.getPrice = function () {
-  return this.price;
-};
-
-/**************** Flower ***************/
-
-function Tree(arg) {
-  Plant.call(this, arg);
-  this.leafs = arg.leafs || null;
-  this.dust_time = arg.dust || null;
-  this.fruits = arg.fruits || null;
-  this.flowers = arg.flowers || null;
-}
-
-inherits(Plant, Tree);
-
-Tree.prototype.isDustingIn = function (month) {
-  return this.dust_time === month ? true : false;
-};
-
-/**************** Flower ***************/
-
-function Flower(arg) {
-  Plant.call(this, arg);
-  this.color = arg.color;
-  this.life_cycle = arg.life_cycle;
-  this.bloom_time = arg.bloom;
-  this.eviroment = arg.env;
-}
-
-inherits(Plant, Flower);
-
-Flower.prototype.isGardenFlower = function () {
-  return this.eviroment.includes('garden') ? true : false;
-};
-
-Flower.prototype.isHomeFlower = function () {
-  return this.eviroment.includes('garden') ? true : false;
-};
-
+//}());
